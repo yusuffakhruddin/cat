5f1786578a4b51a20f2d76a942c8c6921a4c7346
Nur Akhwan <akhwan90@gmail.com>
2015-10-10 11:57:26 +0700

    fix error saat menekan tombol ikuti ujian..

--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -74,7 +74,7 @@ function tambah_jam_sql($menit) {
 	$str = "";
 	if ($menit < 60) {
 		$str = "00:".str_pad($menit, 2, "0", STR_PAD_LEFT).":00";
-	} else if ($menit > 60) {
+	} else if ($menit >= 60) {
 		$mod = $menit % 60;
 		$bg = floor($menit / 60);
 		$str = str_pad($bg, 2, "0", STR_PAD_LEFT).":".str_pad($mod, 2, "0", STR_PAD_LEFT).":00";
